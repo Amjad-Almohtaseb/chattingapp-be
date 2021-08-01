a5170ce18bfbb9b73cc41a9acbb24fcca9b4465e
Amjad-Almohtaseb <amjad.almohtaseb94@gmail.com>
2021-08-01 06:04:19 +0300

    addsocketandfixdatashape

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const { Message } = require("./db/models");
+
 const app = express();
 const passport = require("passport");
 const cors = require("cors");
 const { localStrategy, jwtStrategy } = require("./middleware/passport");
 
+//socket
+const server = require("http").createServer(app);
+const socketio = require("socket.io");
+
+const io = socketio(server, { cors: { origin: "*" } });
 //routes
 const roomsRoutes = require("./routes/roomsRoutes");
 const usersRoutes = require("./routes/usersRoutes");
+
+const messagesRoutes = require("./routes/messagesRoutes");
+
 //middleware
 app.use(cors());
 
@@ -20,7 +30,16 @@ passport.use(jwtStrategy);
 
 app.use(usersRoutes);
 app.use(roomsRoutes);
+app.use(messagesRoutes);
 
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("message", (message) => {
+    Message.create(message);
+
+    socket.emit("message", { message });
+  });
+});
 //error middleware
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
@@ -32,6 +51,6 @@ app.use((req, res, next) => {
   res.status(404).json({ message: "Path not found" });
 });
 
-app.listen(8080, () => {
+server.listen(8080, () => {
   console.log("The application is running on localhost:8080");
 });
